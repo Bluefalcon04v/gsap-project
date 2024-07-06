04bb7042cd116f2c49a9444bb1e5e8499ab23c39
Amit Sah <amitsah04v@gmail.com>
2024-07-06 13:25:11 +0530

    Installed the gsap in project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
-import { Navbar } from "./component/Navbar";
+'use client'
+import gsap from "gsap";
+import {useGSAP} from '@gsap/react'
+import { Navbar } from "./components";
 
 export default function Home() {
+  gsap
+  useGSAP(()=>{
+    gsap.from('#navbar', {
+      x:200, y:400,
+      duration: 1,
+      delay: 2,
+    })
+  })
   return (
     <div className="">
       <Navbar/>
